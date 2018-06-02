28404467e576d90b6d8b929c6f90424e1668bb95
Piyush <Piyush.Dharkar@gmail.com>
2018-06-02 11:49:29 +0530

    Test data is now scaled.

--- Test.py
+++ Test.py
@@ -10,12 +10,15 @@ dataset = pd.read_csv('dataset.csv')
 dataset = dataset.sort_values(by='Date')
 dataset = dataset.drop('Date', axis=1)
 
-test_data = np.array(dataset[4001 : ])
-
-x_test, y_test = process_data(test_data, lookback=10)
-x_test = np.resize(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
 train_data = np.array(dataset[ : 3001])
 
 scaler = fitted_scaler(train_data)
+
+
+test_data = np.array(dataset[4001 : ])
+
+test_data = scaler.transform(test_data)
+
+x_test, y_test = process_data(test_data, lookback=10)
+x_test = np.resize(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
