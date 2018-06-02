792ee863cecc4a931636379bdd3832693552fe0f
Piyush <Piyush.Dharkar@gmail.com>
2018-06-02 11:27:50 +0530

    Reshaped arrays again (added third dimension), trained model.

--- Train.py
+++ Train.py
@@ -28,9 +28,12 @@ val_data = np.array(dataset[3001 : 4001])
 x_train, y_train = process_data(train_data, lookback=10)
 x_val, y_val = process_data(val_data, lookback=10)
 
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+x_val = np.reshape(x_val, (x_val.shape[0], x_val.shape[1], 1))
 
 input_shape = (x_train.shape[1], 1)
-
+batch_size = 100
+epochs = 3
 
 model = Sequential()
 model.add(LSTM(50, input_shape=input_shape))
@@ -39,7 +42,8 @@ model.add(Activation('linear'))
 
 model.compile(optimizer=keras.optimizers.Adam(), loss='mse')
 
-
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,
+          validation_data=(x_val, y_val))
 
 
 
