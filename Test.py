b7f19736c6fe6840f79afe517fb35bf839da3834
Piyush <Piyush.Dharkar@gmail.com>
2018-06-02 12:08:34 +0530

    Cleaned up test.py .

--- Test.py
+++ Test.py
@@ -8,24 +8,24 @@ model = load_model('saved_model.h5')
 
 dataset = pd.read_csv('dataset.csv')
 dataset = dataset.sort_values(by='Date')
-dataset = dataset['Adj Close']
+dataset = np.array(dataset['Adj Close'])
+dataset = np.reshape(dataset, (dataset.shape[0], 1))
 
-train_data = np.array(dataset[ : 3001])
+train_data = dataset[ : 3001]
 
 scaler = fitted_scaler(train_data)
-test_data = np.array(dataset[4001 : ])
+test_data = dataset[4001 : ]
 
 test_data = scaler.transform(test_data)
-test_data = np.reshape(test_data, (test_data.shape[0], 1))
 
 x_test, y_test = process_data(test_data, lookback=10)
 x_test = np.resize(x_test, (x_test.shape[0], x_test.shape[1], 1))
+y_test = np.reshape(y_test, (y_test.shape[0], 1))
 
 y_pred = model.predict(x_test)
 
 y_pred = scaler.inverse_transform(y_pred)
 y_test = scaler.inverse_transform(y_test)
-
 
 print("Actual adjusted closing price")
 print(y_test)
