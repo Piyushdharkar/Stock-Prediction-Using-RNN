6423597eb07d0857c8b6042c6b630e4b5347d2bc
Piyush <Piyush.Dharkar@gmail.com>
2018-06-02 12:28:36 +0530

    Added scaling back to the program, changed train:validation:test proportion. Changed no. of neurons and dropout proportion.

--- Train.py
+++ Train.py
@@ -12,11 +12,11 @@ dataset = dataset.sort_values(by='Date')
 dataset = dataset['Adj Close']
 dataset = np.reshape(dataset, (len(dataset), 1))
 
-#scaler = fitted_scaler(dataset)
-#dataset = scaler.transform(dataset)
+scaler = fitted_scaler(dataset)
+dataset = scaler.transform(dataset)
 
-train_data = np.array(dataset[ : 3001])
-val_data = np.array(dataset[3001 : 4001])  
+train_data = np.array(dataset[ : 4001])
+val_data = np.array(dataset[4001 : 4501])  
 
 x_train, y_train = process_data(train_data, lookback=10)
 x_val, y_val = process_data(val_data, lookback=10)
@@ -26,18 +26,18 @@ x_val = np.reshape(x_val, (x_val.shape[0], x_val.shape[1], 1))
 
 input_shape = (x_train.shape[1], 1)
 batch_size = 100
-epochs = 8
+epochs = 20
 
 model = Sequential()
-model.add(LSTM(100, input_shape=input_shape))
-model.add(Dropout(0.3))
+model.add(LSTM(60, input_shape=input_shape))
+model.add(Dropout(0.8))
 model.add(Dense(1))
 model.add(Activation('linear'))
 
 model.compile(optimizer=keras.optimizers.Adam(), loss='mse')
 
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,
-          validation_data=(x_val, y_val))
+         validation_data=(x_val, y_val))
 
 model.save('saved_model.h5')
 
