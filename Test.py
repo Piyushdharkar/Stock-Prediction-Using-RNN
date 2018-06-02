d34c0d66435d1d901ca5dc18e4badb513a40f064
Piyush <Piyush.Dharkar@gmail.com>
2018-06-02 11:36:47 +0530

    Processed test data.

--- Test.py
+++ Test.py
@@ -1,8 +1,13 @@
 from keras.models import load_model
 import pandas as pd
+from ProcessData import process_data
+import numpy as np
 
 model = load_model('saved_model.h5')
 
 dataset = pd.read_csv('dataset.csv')
 
-test_dataset = dataset[4001 : ]
+test_data = np.array(dataset[4001 : ])
+
+x_test, y_test = process_data(test_data, lookback=10)
+
