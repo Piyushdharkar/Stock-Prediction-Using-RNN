e7f2b83a772644073f9f5a1969d34450b8224fd2
Piyush <Piyush.Dharkar@gmail.com>
2018-06-02 11:12:37 +0530

    Initialized repo. Read and reshaped dataset.

---
+++ Train.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import LSTM, Dropout, Dense, Activation
+import numpy as np
+import keras
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+
+def process_data(data, lookback):
+    X, Y = [], []
+    for i in range(len(data) - lookback - 1):
+        X.append(data[i : (i + lookback), 0])
+        Y.append(data[(i + lookback), 0])
+    return np.array(X), np.array(Y)
+
+dataset = pd.read_csv('dataset.csv')
+dataset = dataset.sort_values(by='Date')
+
+dataset = dataset['Adj Close']
+dataset = np.reshape(dataset, (len(dataset), 1))
+
+train_data = np.array(dataset[ : 3001])
+val_data = np.array(dataset[3001 : 4001])  
+
+x_train, y_train = process_data(train_data, lookback=10)
+x_val, y_val = process_data(val_data, lookback=10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
